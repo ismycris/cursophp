30b968cad72315a38fc96fe18c2044d477b20d51
ismycris <cristielythainara@gmail.com>
2024-01-04 16:08:31 -0300

    update

---
+++ desafios/desafio_003/cad.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>desafio 03</title>
+</head>
+<body>
+    <main>
+        <form action="">
+    <?php 
+    $cotacao = 5.17;
+
+    $real = $_REQUEST["din"] ?? 0;
+    
+    $dolar = $real / $cotacao;
+
+   // $padrao = numfmt_create("pt_br", NumberFormatter::CURRENCY);
+    $real_formatado = number_format($real, 2, ',', '.');
+    $dolar_formatado = number_format($dolar, 2, ',', '.');
+
+    echo " R$ $real_formatado reais é equivalente a $dolar_formatado dólares com a taxa de câmbio de $cotacao.";
+    ?>
+</form>
+    </main>
+</body>
+</html>
