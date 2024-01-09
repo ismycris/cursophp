a195f84a437132f4d183890896a579f5269fded0
ismycris <cristielythainara@gmail.com>
2024-01-09 10:11:41 -0300

    update

--- exer005/globais.php
+++ exer005/globais.php
@@ -10,6 +10,12 @@
     <main>
         <pre>
             <?php 
+
+            setcookie("dia-da-semana","SEGUNDA",time()+ 3600);
+
+            session_start();
+            $_SESSION["teste"]= "funcionou!";
+
             echo" <h1> Superglobal Get </h1>";
             var_dump($_GET);
 
@@ -18,6 +24,24 @@
 
             echo"<h1> Seuperglobal REQUEST </h1>";
             var_dump($_REQUEST);
+
+            echo"<h1> Seuperglobal COOKIE </h1>";
+            var_dump($_COOKIE);
+
+            echo"<h1> Seuperglobal SESSION </h1>";
+            var_dump($_SESSION);
+
+            echo"<h1> Seuperglobal ENV </h1>";
+            var_dump($_ENV);
+           // foreach(getenv() as $c => $v){
+          //      echo"$c -> $v <br>";
+          //  }
+            echo"<h1> Seuperglobal SERVER </h1>";
+            var_dump($_SERVER);
+
+            echo"<h1> Seuperglobal globals </h1>";
+            var_dump($GLOBALS);
+
             ?>
 
         </pre>
---
+++ exer006/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    //CAPTURANDO DA DADOS DO formulario retroalimentado
+    $valor1 =  $_GET['v1'] ?? 0;
+    $valor2 = $_GET['v2'] ?? 0;
+    ?>
+    <main>
+        <h1>Somador de valores</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+
+        <label for="v1">valor 1</label>
+        <input type="number" name="v1" id="v1" value="<?=$valor1?>">
+        <label for="v2">valor 2</label>
+        <input type="number" name="v2" id="v2" value="<?=$valor2?>">
+        <input type="submit" value="somar">
+
+        </form>
+    </main>
+    <section id="Resultado">
+        <h2>Resultado da soma</h2>
+        <?php 
+            $soma = $valor1 + $valor2;
+            echo"<p> a soma entre os valores $valor1 e $valor2 e igual a $soma </p>";
+        ?>
+    </section>
+</body>
+</html>
